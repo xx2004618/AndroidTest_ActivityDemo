09643333b834c0b70bad7d9e5722ad0fd4ccb401
xx2004618 <xx2004618@qq.com>
2016-08-30 16:41:55 -0700

    Work normally. The main activity page has two buttons. The first is to call the second activity directly. The second calls the second activity and get return value from it.

--- app/src/main/java/xiangxiao/activitydemo/MainActivity.java
+++ app/src/main/java/xiangxiao/activitydemo/MainActivity.java
@@ -1,25 +1,53 @@
 package xiangxiao.activitydemo;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ContentFrameLayout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    private Button button;
+    private Button btn1;
+    private Button btn2;
+    private Context mContext;
+    private TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.btn1);
-        button.setOnClickListener(new View.OnClickListener() {
+        mContext = this;
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                Intent intent = new Intent(mContext, SecondActivity.class);
                 startActivity(intent);
             }
         });
+
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, SecondActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == 2) {
+            String content = data.getStringExtra("data");
+            tv = (TextView) findViewById(R.id.text1);
+            tv.setText(content);
+        }
     }
 }
--- app/src/main/java/xiangxiao/activitydemo/SecondActivity.java
+++ app/src/main/java/xiangxiao/activitydemo/SecondActivity.java
@@ -1,17 +1,35 @@
 package xiangxiao.activitydemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by xiangxiao on 2016/8/30.
  */
 public class SecondActivity extends Activity {
+    private Button btn;
+    private String result = "Hello";
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        btn = (Button) findViewById(R.id.btn3);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent();
+                data.putExtra("data", result);
+                setResult(2, data);
+
+                finish();
+            }
+        });
     }
 }
